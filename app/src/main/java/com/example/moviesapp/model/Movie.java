653dea0d9d17b11ffe8358df02d75fc9bf85aadd
Laimonas Vaitkus <laimonasvaitkus.lv@gmail.com>
2022-09-30 11:54:09 +0100

    Initial commit/ model

---
+++ app/src/main/java/com/example/moviesapp/model/Movie.java
@@ -0,0 +1,227 @@
+package com.example.moviesapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.library.baseAdapters.BR;
+
+import java.util.List;
+import javax.annotation.Generated;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Movie extends BaseObservable implements Parcelable {
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("adult")
+    @Expose
+    private Boolean adult;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Integer> genreIds = null;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @SerializedName("original_language")
+    @Expose
+    private String originalLanguage;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("popularity")
+    @Expose
+    private Double popularity;
+    @SerializedName("vote_count")
+    @Expose
+    private Integer voteCount;
+    @SerializedName("video")
+    @Expose
+    private Boolean video;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+
+    // Bindable has to be before every get, after placing the Bindable, rebuild project is needed
+    // notifyPropertyChanged needs to be placed at the end of the set
+    @Bindable
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+        notifyPropertyChanged(BR.posterPath);
+    }
+
+    @Bindable
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+        notifyPropertyChanged(BR.adult);
+    }
+
+    @Bindable
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+        notifyPropertyChanged(BR.overview);
+    }
+
+    @Bindable
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+        notifyPropertyChanged(BR.releaseDate);
+    }
+
+    @Bindable
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+        notifyPropertyChanged(BR.genreIds);
+    }
+
+    @Bindable
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+
+    @Bindable
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+        notifyPropertyChanged(BR.originalTitle);
+    }
+
+    @Bindable
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+        notifyPropertyChanged(BR.originalLanguage);
+    }
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notifyPropertyChanged(BR.title);
+    }
+
+    @Bindable
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+        notifyPropertyChanged(BR.backdropPath);
+    }
+
+    @Bindable
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+        notifyPropertyChanged(BR.popularity);
+    }
+
+    @Bindable
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+        notifyPropertyChanged(BR.voteCount);
+    }
+
+    @Bindable
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+        notifyPropertyChanged(BR.video);
+    }
+
+    @Bindable
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+        notifyPropertyChanged(BR.voteAverage);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeValue(voteCount);
+        parcel.writeValue(id);
+        parcel.writeValue(video);
+        parcel.writeValue(voteAverage);
+        parcel.writeValue(title);
+        parcel.writeValue(popularity);
+        parcel.writeValue(posterPath);
+        parcel.writeValue(originalLanguage);
+        parcel.writeValue(originalTitle);
+        parcel.writeList(genreIds);
+        parcel.writeValue(backdropPath);
+        parcel.writeValue(adult);
+        parcel.writeValue(overview);
+        parcel.writeValue(releaseDate);
+    }
+}
