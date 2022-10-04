dd4fc18602cdaff974b85a5c2bb154c2d86ad311
Laimonas Vaitkus <laimonasvaitkus.lv@gmail.com>
2022-10-04 20:47:30 +0100

    Adapter

--- app/src/main/java/com/example/moviesapp/view/MovieActivity.java
+++ app/src/main/java/com/example/moviesapp/view/MovieActivity.java
@@ -1,16 +1,41 @@
 package com.example.moviesapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.example.moviesapp.R;
+import com.example.moviesapp.databinding.ActivityMovieBinding;
+import com.example.moviesapp.model.Movie;
 
 public class MovieActivity extends AppCompatActivity {
+
+    // binding of activity movie xml
+    private Movie movie;
+    private ActivityMovieBinding activityMovieBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        activityMovieBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie);
+
+        Intent i = getIntent();
+
+        if (i != null){
+            movie = getIntent().getParcelableExtra("movie");
+            activityMovieBinding.setMovie(movie);
+            getSupportActionBar().setTitle(movie.getTitle());
+        }
+
     }
 }
